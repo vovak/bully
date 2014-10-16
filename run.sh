1e03d41250391b69590380c35a09922036fe51be
Vladimir Kovalenko <vladimir.kovalenko@jetbrains.com>
2014-10-17 03:49:39 +0400

    working impl

--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-N=3
+N=5
 NODES=""
 for (( i=0; i<$N; i++ ))
 do
     NODES+=" bully$i@`hostname`"
+done
+
+for (( i=0; i<$N; i++ ))
+do
     erl -pa out/production/bully -name "bully$i@`hostname`" -s bully start ${NODES} -noshell &
-    sleep 1
+#    sleep 1
 done
 read
 pkill beam.smp
--- src/bully.erl
+++ src/bully.erl
@@ -12,51 +12,81 @@
 %% API
 -export([start/1]).
 
--define(ELECTION_MESSAGE, election).
--define(ELECTION_MESSAGE_RESPONSE, ok).
--define(COORDINATOR_MESSAGE, imtheboss).
--define(RESPONSE_TIMEOUT, 20).
+-define(ELECTION_MESSAGE, 'ELEC').
+-define(ELECTION_MESSAGE_RESPONSE, 'OKAY').
+-define(COORDINATOR_MESSAGE, 'BOSS').
+-define(RESPONSE_TIMEOUT, 10).
 
 -record(state, {timeout = infinity, knownnodes = [], coordinator = node()}).
 
 start(Nodes) ->
   register(?MODULE, self()),
-  io:format("Node ~s has a PId of ~s.~n", [node(), os:getpid()]).
-%%   loop(greetNodes(#state{knownnodes = Nodes})).
+  io:format("Node ~s has a PId of ~s.~n", [node(), os:getpid()]),
+  loop(startElection(#state{knownnodes = Nodes}, Nodes)).
 
 loop(State) ->
   Timeout = State#state.timeout,
+  Coordinator = State#state.coordinator,
   NewState = receive
                {?ELECTION_MESSAGE, Node} -> handleElectionMessage(State, Node);
-               {?ELECTION_MESSAGE_RESPONSE, Node} -> waitForCoordinatorMessage(State);
-               {?COORDINATOR_MESSAGE, Node} -> setCoordinator(State, Node)
+               {?ELECTION_MESSAGE_RESPONSE, _} -> waitForCoordinatorMessage(State);
+               {?COORDINATOR_MESSAGE, Node} -> setCoordinator(State, Node);
+               {nodedown, Coordinator} -> setCoordinator(State,node()), startElection(State, State#state.knownnodes);
+               {nodedown, _} -> State
              after
                Timeout -> becomeCoordinator(State)
              end,
-  io:format("~s reached loop end~n", [node()]),
   loop(NewState).
 
-handleElectionMessage(State, Node) ->
-  State.
 
-waitForCoordinatorMessage(State) ->
+startElection(State, Nodes) ->
+  lists:foreach(fun sendElectionMessage/1, higherIds(Nodes)),
   NewState = State#state{timeout = ?RESPONSE_TIMEOUT},
   NewState.
 
-setCoordinator(State, Node) ->
-  State.
+sendElectionMessage(Node) ->
+  sendMessageToNode(Node, ?ELECTION_MESSAGE).
 
-log(State, Node) ->
-  io:format("~s received something from node ~s~n", [node(), atom_to_list(Node)]),
-  State.
+sendOkMessage(Node) ->
+  sendMessageToNode(Node, ?ELECTION_MESSAGE_RESPONSE).
 
-addKnownNode(State, Node) ->
-  io:format("Node ~s received a message from previously unknown node ~s~n,", [node(), atom_to_list(Node)]),
-  NewState = #state{knownnodes = lists:append([State#state.knownnodes,[Node]])},
+handleElectionMessage(State, Node) ->
+  HigherNodes = higherIds(State#state.knownnodes),
+  if
+    length(HigherNodes) == 0 -> becomeCoordinator(State);
+    true -> sendOkMessage(Node), startElection(State, State#state.knownnodes)
+  end.
+
+waitForCoordinatorMessage(State) ->
+  NewState = State#state{timeout = infinity},
   NewState.
 
+setCoordinator(State,Node) ->
+%%   monitor_node(#state.coordinator, false),
+  monitor_node(Node, true),
+  NewState = State#state{coordinator = Node, timeout = infinity},
+  io:format("Node ~s now thinks ~s is the leader~n", [atom_to_list(node()), Node]),
+  NewState.
+
+becomeCoordinator(State) ->
+  setCoordinator(State, node()),
+  NewState = State#state{timeout = infinity},
+  broadcastCoordinatorMessage(State),
+  NewState.
+
+broadcastCoordinatorMessage(State) ->
+  lists:foreach(fun sendCoordinatorMessage/1, lowerIds(State#state.knownnodes)).
+
+sendCoordinatorMessage(Node) ->
+  sendMessageToNode(Node, ?COORDINATOR_MESSAGE).
+
+higherIds(Nodes) ->
+  lists:filter(fun(Node) -> Node > node() end, Nodes).
+
+lowerIds(Nodes) ->
+  lists:filter(fun(Node) -> Node < node() end, Nodes).
 
 sendMessageToNode(Node, Message) ->
-  io:format("Node ~s is sending message ~s to node ~s~n", [node(), Message, atom_to_list(Node)]),
+  io:format("~s >>>>> ~s >>>>> ~s~n", [node(), Message, atom_to_list(Node)]),
   {?MODULE, Node} ! {Message, node()}.
 
